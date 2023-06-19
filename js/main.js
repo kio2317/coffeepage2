4f7a436b988e13a0e4b516a49a94794a5903fa9c
kio2317 <kio2317@naver.com>
2023-06-19 15:10:33 +0900

    banner1~3완료

--- js/main.js
+++ js/main.js
@@ -100,3 +100,18 @@ gsap.to('.floating3', 2.5, {
   yoyo: true, // 한 번 재생된 애니메이션을 다시 뒤로 재생
   ease: Power1.easeInOut // Easing 함수 적용
 });
+/**
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
+const spyEls = document.querySelectorAll('section.scroll-spy');
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
